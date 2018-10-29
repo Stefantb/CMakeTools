191280e12721269f78474a9a94c6f09352a9a175
Stefán Þór Bjarnason <stefan.bjarnason@marel.com>
2018-10-29 22:57:21 +0000

    Able to change generator based on CMake capabilities.

---
+++ change_config_setting.py
@@ -0,0 +1,37 @@
+# import json
+
+import sublime_plugin
+
+from . import cmake_server
+from . import project_settings as ps
+
+
+# *****************************************************************************
+#
+# *****************************************************************************
+def change_generator(settings, window):
+    caps = cmake_server.get_capabilities(settings.get_multilevel_setting('cmake_binary'))
+    generators = [generator.get('name') for generator in caps['generators']]
+
+    def selected(id):
+        print(generators[id])
+        settings.current_configuration.generator = generators[id]
+        settings.commit()
+
+    window.show_quick_panel(generators, selected)
+
+
+# *****************************************************************************
+#
+# *****************************************************************************
+class CmakeideChangeConfigSettingCommand(sublime_plugin.WindowCommand):
+
+    def run(self, *args, setting=None, **kwargs):
+
+        if setting is None:
+            return
+
+        settings = ps.CmakeIDESettings(self.window)
+
+        if setting == 'generator':
+            change_generator(settings, self.window)
